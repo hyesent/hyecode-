bfed2608f4c76d8b17f3fff0de54fc94649dc826
hyesent <hyacinthmichael36@gmail.com>
2026-06-07 01:36:33 +0100

    Change editor height from 100% to 600px

--- src/App.jsx
+++ src/App.jsx
@@ -756,7 +756,7 @@ export default function HyecodeEditor() {
           <div className="flex-1 bg-[#0a0a0a] overflow-hidden">
             {view === 'code'? (
               <Editor
-                height="100%"
+                height="600px"
                 width="100%"
                 path={activeFile?.name}
                                 language={activeFile?.lang || 'javascript'}
